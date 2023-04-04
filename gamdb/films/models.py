fec6947ae6ce15f253ab23116a4dd236ef54bb83
Fravoj <83371002+Fravoj3@users.noreply.github.com>
2023-04-04 11:19:28 +0200

    Přidáni režizéři

--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=200)
-    year = models.IntegerField()
-    description = models.TextField()
-
-    
+    year = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    gengere = models.ManyToManyField('Genere', blank=True, null=True)   
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+class Director(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField(blank=True, null=True)
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+class Genere(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
--- gamdb/gamdb/urls.py
+++ gamdb/gamdb/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from films.views import homepage, directors
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage),
+    path('directors', directors)
 ]
